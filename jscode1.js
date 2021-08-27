a476664f0c9ac27620a9b7ad0d7b69960462c00e
hirak-dutta-au6 <hirakdutta2010@gmail.com>
2021-08-27 03:39:11 -0700

    js tutorial 2nd day

---
+++ jscode1.js
@@ -0,0 +1,26 @@
+
+// js code tutorial 2nd day
+
+
+// data types
+// boolean datatypes
+var ok= true;
+var notDone= false;
+console.log(notDone); // here notDone will be printed in console
+
+
+//string datatypes
+var str =" this is a double quote string";
+var str1 = 'this is a single quote string';
+console.log(str);
+console.log(str1);
+
+//undefined datatypes
+
+var k;
+console.log(k);  // here k will be undefined
+
+//null datatypes
+
+var i= null;    
+
