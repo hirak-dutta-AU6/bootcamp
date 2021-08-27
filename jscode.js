af6afc9e926565035041cd2d65fe4035212b732e
hirak-dutta-au6 <hirakdutta2010@gmail.com>
2021-08-27 03:12:09 -0700

    created a first js tutorial

---
+++ jscode.js
@@ -0,0 +1,23 @@
+
+// MY FIRST JS TUTORIAL
+//check in console
+console.log("hello world");
+
+
+
+// arithmetic operation
+ var x=5;
+ var y=15;
+ var z= x+y;
+ console.log(z);
+ console.log(x*y);
+ console.log(x/y);
+ console.log(x%y);
+ console.log(2**3);
+ 
+
+ var x=100;
+ console.log(x);
+
+
+
