7c8a1b76db928009d7ed51a9b8fd272917f02ca2
hirak-dutta-au6 <hirakdutta2010@gmail.com>
2021-08-27 03:27:10 -0700

    few lines edited

--- jscode.js
+++ jscode.js
@@ -8,12 +8,12 @@ console.log("hello world");
 // arithmetic operation
  var x=5;
  var y=15;
- var z= x+y;
- console.log(z);
- console.log(x*y);
- console.log(x/y);
- console.log(x%y);
- console.log(2**3);
+ var z= x+y;         // addition operation
+ console.log(z);         
+ console.log(x*y);   //  multiplication
+ console.log(x/y);    // division
+ console.log(x%y);     //modulus
+ console.log(2**3);    //power
  
 
  var x=100;
