2bb61b1b4681dbb2d21632d64d83798e1f52fd32
hirak-dutta-au6 <hirakdutta2010@gmail.com>
2021-08-27 04:12:03 -0700

    js tutorials2 continues

---
+++ jscode2.js
@@ -0,0 +1,22 @@
+//js tutorial 2 day continues...
+// increment and decrement operator
+var a = 10;
+console.log(++a * 10);
+console.log(a);       //only after incrementing the variable will participate in any arithmetic operation
+
+// decrement operator
+  var h=100;
+  console.log(--h); //here h= h-1
+  console.log(-40/0);  // infinity
+    
+
+  // some others operators
+  //!---   not operator(basically output here is  true or false)
+
+  console.log(!true)     // false
+  console.log(!false);    // true
+  console.log(!0);        //true
+  console.log(!undefined);  //true
+  // in js 0 means false, false , undefined ---- all are falsey value
+
+  
