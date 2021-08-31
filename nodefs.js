ba671026b30949fb46d52a05d1073ce6e83bf617
hirak-dutta-au6 <hirakdutta2010@gmail.com>
2021-08-31 06:23:09 -0700

    added few lines of code

--- nodefs.js
+++ nodefs.js
@@ -2,8 +2,9 @@
 
 
 var fs = require("fs");
-
+console.log(1);
 fs.readFile("sample.txt",function(err,data){
 console.log("error is ...."+err);
 console.log("Data is ...."+data);
 });
+console.log(2);
